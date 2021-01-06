9c4fe541fcb02db9c870e3cda6e0143eeb2d647d
J3SUNG <wptjd6141@naver.com>
2021-01-06 08:37:05 -0800

    Add Router

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZaHakdsu7VCrB18zZMdCgGq_id5USBt8",
-  authDomain: "twitter-db805.firebaseapp.com",
-  projectId: "twitter-db805",
-  storageBucket: "twitter-db805.appspot.com",
-  messagingSenderId: "1038849744653",
-  appId: "1:1038849744653:web:f5d1103ed666a348db1a44",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
