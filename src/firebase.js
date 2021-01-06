08f7310309ffd75f97e0e0730460de2bea0a475b
J3SUNG <wptjd6141@naver.com>
2021-01-06 07:55:00 -0800

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZaHakdsu7VCrB18zZMdCgGq_id5USBt8",
+  authDomain: "twitter-db805.firebaseapp.com",
+  projectId: "twitter-db805",
+  storageBucket: "twitter-db805.appspot.com",
+  messagingSenderId: "1038849744653",
+  appId: "1:1038849744653:web:f5d1103ed666a348db1a44",
+};
+
+export default firebase.initializeApp(firebaseConfig);
