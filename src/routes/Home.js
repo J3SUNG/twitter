281188b0c0f3ff8ca205881cd242d4fe54a995a3
J3SUNG <wptjd6141@naver.com>
2021-01-06 22:13:00 -0800

    Home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={tweet}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={20}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
