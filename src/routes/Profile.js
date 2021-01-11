5031490464986727f7dd9fda53cd9e4e88eaa5c9
J3SUNG <wptjd6141@naver.com>
2021-01-11 10:28:35 -0800

    filter

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import { useHistory } from "react-router-dom";
-import { authService } from "../fbase";
+import { authService, dbService } from "../fbase";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   // const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     // history.push("/");
   };
+  const getMyTweets = async () => {
+    const tweets = await dbService
+      .collection("tweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(tweets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMyTweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
