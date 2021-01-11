e6606dc4f8983705ae0c06719265055cad0e3813
J3SUNG <wptjd6141@naver.com>
2021-01-11 12:14:32 -0800

    update profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,12 +1,10 @@
-import React, { useEffect } from "react";
-// import { useHistory } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import { authService, dbService } from "../fbase";
 
 const Profile = ({ userObj }) => {
-  // const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
-    // history.push("/");
   };
   const getMyTweets = async () => {
     const tweets = await dbService
@@ -19,8 +17,31 @@ const Profile = ({ userObj }) => {
   useEffect(() => {
     getMyTweets();
   }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
